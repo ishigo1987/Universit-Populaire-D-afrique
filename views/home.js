52da46b372aad2d9bf7f49abd102af973fd358f3
ishigo1987 <stephnguets@yahoo.fr>
2017-07-17 17:15:03 +0100

    changement mineur

--- views/home.js
+++ views/home.js
@@ -219,7 +219,7 @@ exports.create = () => {
                 localStorage.removeItem("categoryQuery");
             } else if (tabActive === "Quizz") {
                 // Ajax quizz categorie
-                let url = `https://www.upa.ovh/gestionApplication/retrieveQuizzToDb.php?categorie=${localStorage.getItem("categoryQuery")}`;
+                let urlQuizz = `https://www.upa.ovh/gestionApplication/retrieveQuizzToDb.php?categorie=${localStorage.getItem("categoryQuery")}`;
                 require("../modules/ajaxQuizz.js")(tabQuizz, urlQuizz, scrollViewTabQuizz, themeColor, executeNavigationView);
                 localStorage.removeItem("categoryQuery");
             } else {
