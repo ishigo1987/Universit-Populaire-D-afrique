74aed2170c9b9727110ecbf08a630ff6c08917c7
ishigo1987 <stephnguets@yahoo.fr>
2017-07-27 17:05:17 +0100

    Changement de la creation du code de verification

--- modules/sendVerificationCode.js
+++ modules/sendVerificationCode.js
@@ -1,14 +1,15 @@
-module.exports = (emailTosendCode,whatViewAskVerificationCode) =>
- {
+module.exports = (emailTosendCode, whatViewAskVerificationCode) => {
     "use strict";
     let generateNumber = new Uint16Array(1);
     window.crypto.getRandomValues(generateNumber);
-    let codeVerification = generateNumber[0];
-    let jsonToSend = 
-     {
-        email:emailTosendCode,
-        code:codeVerification
-     }
-    localStorage.setItem("verificationCode", String(codeVerification));
-    let StatusSendVerificationCode = require("./ajax.js")(jsonToSend,"Code de verification",whatViewAskVerificationCode,undefined);
- };
+    let codeVerification = String(generateNumber[0]);
+    if (codeVerification.length === 4) {
+        codeVerification = `5${codeVerification}`;
+    }
+    let jsonToSend = {
+        email: emailTosendCode,
+        code: codeVerification
+    }
+    localStorage.setItem("verificationCode", codeVerification);
+    let StatusSendVerificationCode = require("./ajax.js")(jsonToSend, "Code de verification", whatViewAskVerificationCode, undefined);
+};
